fc92e39ae8160c822849f4cf1e10c5cdfd0ea53e
Tomisin <oyemadeoluwatomisin75@gmail.com>
2023-12-05 17:18:23 +0100

    full changes

--- rehoboth-hills/src/components/project/project.tsx
+++ rehoboth-hills/src/components/project/project.tsx
@@ -99,28 +99,4 @@ const PROJECT: React.FC<ProjectProps> = ({
 }
 
 export default PROJECT
-//<div className=" bg-[#FFF] w-[487px] px-[22px] pt-[40px] pb-[47px] absolute -right-[25%]  top-[140px] border-[0.5px]">
-{/* <img src={Project} alt={headT} /> */ }
-{/* <div className="bg-[#FFF] mt-6 first:m-0">
-          <h2 className="text-xl md:text-[32px] text-[#111]">{headT}</h2>
-          <p className="font-light text-[14px] mb-6 w-[250px]">{text}</p>
-          <h6 className="text-xs font-semibold">{sHeadT}</h6>
-          <ol className="list-decimal  font-light text-sm w-[280px] leading-normal ml-4 mb-6">
-            <li>{list_one}</li>
-            <li>{list_two}</li>
-            <li>{list_three}</li>
-          </ol>
-          <div className=" mb-1">
-            <span className=" font-light text-[15px]">Location:</span>
-            <span className=" font-medium text-[15px]">{location}</span>
-          </div>
-          <div className=" mb-1">
-            <span className=" font-light text-[15px]">Price:</span>
-            <span className=" font-medium text-[15px]">{price}</span>
-          </div>
-          <div className=" my-8">
-            <a href="" className="border-b pb-1 border-[#00000080]" >BUY NOW</a>
 
-          </div>
-
-        </div> */}
