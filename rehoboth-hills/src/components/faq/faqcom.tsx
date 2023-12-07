67209124492a5e1072a21434e645d38f579acdcd
Tomisin <oyemadeoluwatomisin75@gmail.com>
2023-12-07 23:54:49 +0100

    ...

--- rehoboth-hills/src/components/faq/faqcom.tsx
+++ rehoboth-hills/src/components/faq/faqcom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./faq.module.scss";
 import { MinSvg } from "@/assets/svg/SvgIcons";
 import { PlusSvg } from "@/assets/svg/SvgIcons";
