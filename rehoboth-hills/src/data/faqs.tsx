3c4c43ab37a5345e5c62ee13ea92ee5c6dbf7ad1
Tomisin <oyemadeoluwatomisin75@gmail.com>
2023-12-08 09:19:02 +0100

    FAQ updates

--- rehoboth-hills/src/data/faqs.tsx
+++ rehoboth-hills/src/data/faqs.tsx
@@ -7,22 +7,22 @@ export const faqcoms = [
   {
     question: "What is Anora City?",
     answer:
-      "In an ever-evolving real estate landscape, Rehoboth Hills International Limited stands out as a beacon of unwavering commitment to crafting not just homes, but lifestyles. We understand that a home isn't merely a structure but a reflection of your dreams and aspirations. With this core philosophy, we've embarked on a journey to create spaces that seamlessly blend modernity, sustainability, and luxury.",
+      "Anora city is situated in KETU EPE , it is a place where luxury meets tranquility, and every corner tells a story of radiant urban living.  It boasts several notable landmarks that contribute to  investment potential such as Africa’s Largest Food & Logistics Hub, Lagos Film City, Epe Resort & Spa , Alaro City, Isimi Lagos and Augustine University .",
   },
   {
     question: "What are our Values?",
     answer:
-     "In an ever-evolving real estate landscape, Rehoboth Hills International Limited stands out as a beacon of unwavering commitment to crafting not just homes, but lifestyles. We understand that a home isn't merely a structure but a reflection of your dreams and aspirations. With this core philosophy, we've embarked on a journey to create spaces that seamlessly blend modernity, sustainability, and luxury."
+     "Our commitment to values is evident in every transaction and deal. We uphold the principles of integrity, excellence, accountability, credibility, innovation, and security, ensuring that our clients experience the highest standards of professionalism and trustworthiness in their interactions with us."
   },
   {
     question: "How can i Invest in Anora city?",
     answer:
-      "In an ever-evolving real estate landscape, Rehoboth Hills International Limited stands out as a beacon of unwavering commitment to crafting not just homes, but lifestyles. We understand that a home isn't merely a structure but a reflection of your dreams and aspirations. With this core philosophy, we've embarked on a journey to create spaces that seamlessly blend modernity, sustainability, and luxury.",
+      "Unlock the doors to Anora City with an initial deposit of ₦300,000. Choose from our meticulously designed plots, available in 300SQM and 500SQM for ₦‎1.5m and ₦‎2.5m respectively. Contact us today and embark on the journey to become a proud landowner in one of the fastest-growing cities in Africa.",
   },
   {
     question: "What we do?",
     answer:
-      "In an ever-evolving real estate landscape, Rehoboth Hills International Limited stands out as a beacon of unwavering commitment to crafting not just homes, but lifestyles. We understand that a home isn't merely a structure but a reflection of your dreams and aspirations. With this core philosophy, we've embarked on a journey to create spaces that seamlessly blend modernity, sustainability, and luxury.",
+      "At Rehoboth Hills International Limited, we provide unparalleled real estate solutions tailored for individuals and businesses globally. Our mission is to redefine wealth creation in the realm of real estate, setting new standards for excellence and innovation.",
   },
 ];
 
